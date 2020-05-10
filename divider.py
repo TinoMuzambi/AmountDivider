e7889d0e6f6a08dc21265bf2cf809f16e5fa1b65
Tino <tinomuzambi@gmail.com>
2020-05-10 15:19:18 +0200

    Implements initial logic.

--- divider.py
+++ divider.py
@@ -1,8 +1,20 @@
 # Tino Muzambi
+import sys
+
+
+def get_bins(full_amount, no_bins):
+    bins = []
+    first_ratio = no_bins - 2 # TODO set this dynamically.
+    for i in range(no_bins):
+        bins.append(full_amount * (first_ratio / 10))
+        first_ratio -= no_bins / 10
+    return bins
 
 
 def main():
-    print(0)
+    full_amount = int(sys.argv[1])
+    num_bins = int(sys.argv[2])
+    print(get_bins(full_amount, num_bins))
 
 
 if __name__ == '__main__':
